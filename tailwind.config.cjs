4d4bb75cb2d7710b5d5c246b9caa740ddbce9612
surya vivek <suryavivek33377@gmail.com>
2022-12-10 18:16:58 +0530

    mobile home page markup and styles completed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				homeBgMobile:
+					"url('/src/assets/home/background-home-mobile.jpg')",
+				homeBgDesktop:
+					"url('/src/assets/home/background-home-desktop.jpg')",
+				homeBgTablet:
+					"url('/src/assets/home/background-home-tablet.jpg')",
+			},
+		},
+		colors: {
+			transparent: "transparent",
+			current: "currentColor",
+			darkBlue: "#0B0D17",
+			voilet: "#D0D6F9",
+			white: "#ffffff",
+		},
+		fontFamily: {
+			bellefair: ["Bellefair", "serif"],
+			barlow: ["Barlow Condensed", "sans-serif"],
+		},
 	},
 	plugins: [],
-}
+};
