27f7d656929919aeced9abf5ab0d1cd8cf7bbe96
surya vivek <suryavivek33377@gmail.com>
2023-01-06 21:55:26 +0530

    destination page functions

---
+++ src/components/DestinationDesc.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { destination } from "../utils/svelte/destinationStore";
+</script>
+
+<h1>{$destination.title}</h1>
+<p>{$destination.desc}</p>
+<h3>{$destination.distance}</h3>
+<h3>{$destination.travelTime}</h3>
